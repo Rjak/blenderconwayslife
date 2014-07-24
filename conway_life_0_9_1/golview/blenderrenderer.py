5f8d14b1fc54b9d5516c8a64c02bdf8067e6d5c0
Joe Howes <joeh@foldingrain.com>
2014-07-24 18:39:06 -0500

    General improvements.
    - Add GPL block.
    - Fix line continuation typo.

--- conway_life_0_9_1/golview/blenderrenderer.py
+++ conway_life_0_9_1/golview/blenderrenderer.py
@@ -1,3 +1,20 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
 import configparser
 
 from golmodel.lifeform import LifeForm
@@ -9,7 +26,7 @@ from .golrenderer import GOLRenderer
 class BlenderRenderer(GOLRenderer):
     """Renders mesh instances and keyframes to Blender"""
 
-    BLENDER_CONFIG_FILE = "%s%sblenderrenderer.cfg" %
+    BLENDER_CONFIG_FILE = "%s%sblenderrenderer.cfg" % \
       (os.path.dirname(__file__), os.sep)
 
     def __init__(self, universe):
